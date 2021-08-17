cdffda89b914c04c2c2bd67ae49730b16474bfa4
Yiming Gong <ymgong@umich.edu>
2021-08-17 17:01:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 import streamlit as st
-x = st.slider('x')
-st.write(x, 'squared is', x * x)
+import pandas as pd
+
+# Reuse this data across runs!
+read_and_cache_csv = st.cache(pd.read_csv)
+
+BUCKET = "https://streamlit-self-driving.s3-us-west-2.amazonaws.com/"
+data = read_and_cache_csv(BUCKET + "labels.csv.gz", nrows=1000)
+desired_label = st.selectbox('Filter to:', ['car', 'truck'])
+st.write(data[data.label == desired_label])
