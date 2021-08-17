6989af667c1d3f778a042286c73a0a2af6380cd6
Yiming Gong <ymgong@umich.edu>
2021-08-17 14:45:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,35 @@
+ifrom vega_datasets import data
 import streamlit as st
-import pandas as pd
+import altair as alt
 
-# Reuse this data across runs!
-read_and_cache_csv = st.cache(pd.read_csv)
+def main():
+    df = load_data()
+    page = st.sidebar.selectbox("Choose a page", ["Homepage", "Exploration"])
 
-BUCKET = "https://streamlit-self-driving.s3-us-west-2.amazonaws.com/"
-data = read_and_cache_csv(BUCKET + "labels.csv.gz", nrows=1000)
-desired_label = st.selectbox('Filter to:', ['car', 'truck'])
-st.write(data[data.label == desired_label])
-view raw
+    if page == "Homepage":
+        st.header("This is your data explorer.")
+        st.write("Please select a page on the left.")
+        st.write(df)
+    elif page == "Exploration":
+        st.title("Data Exploration")
+        x_axis = st.selectbox("Choose a variable for the x-axis", df.columns, index=3)
+        y_axis = st.selectbox("Choose a variable for the y-axis", df.columns, index=4)
+        visualize_data(df, x_axis, y_axis)
+
+@st.cache
+def load_data():
+    df = data.cars()
+    return df
+
+def visualize_data(df, x_axis, y_axis):
+    graph = alt.Chart(df).mark_circle(size=60).encode(
+        x=x_axis,
+        y=y_axis,
+        color='Origin',
+        tooltip=['Name', 'Origin', 'Horsepower', 'Miles_per_Gallon']
+    ).interactive()
+
+    st.write(graph)
+
+if __name__ == "__main__":
+    main()
